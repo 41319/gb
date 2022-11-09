67065d491fc18c3b55f7d3d2328048df5aa5d890
cheehau <puahcheehau@gmail.com>
2022-11-09 04:54:24 +0000

    Update main to output generated at 967e239

---
+++ assets/js/fb.js
@@ -0,0 +1,8 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '2479203505722557',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v15.0'
+    });
+  };
