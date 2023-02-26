ec29f103062f2aad7aee6940981b171abf89ede8
41319 <puahcheehau@gmail.com>
2023-02-26 09:43:51 +0000

    Update main to output generated at ec287d1

--- assets/js/fb.js
+++
@@ -1,8 +0,0 @@
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '2479203505722557',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v15.0'
-    });
-  };
